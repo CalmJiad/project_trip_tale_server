81a28d0ab340ca755bc841123bdc2a2de6789a4f
calmjiad <unlucky4246@gmail.com>
2021-10-31 17:55:11 +0600

    server side implementation done

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmv2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tripTale");
+    const servicesCollection = database.collection("services");
+    const usersCollection = database.collection("users");
+
+    //POST Api to post services data
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      res.json(result);
+    });
+
+    //POST Api to post user booking data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // Get api to all orders data
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const orderedData = await cursor.toArray();
+      res.send(orderedData);
+    });
+
+    //GET Api to get all services data
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET api to get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    // Delete api to cancel orderd events
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello i am running the triptale server");
+});
+
+app.listen(port, () => {
+  console.log("this is running", port);
+});
